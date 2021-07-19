b7f16d688e06050978f0d8a2e4889d5453857864
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2021-07-18 18:06:52 -0700

    coding

---
+++ train.RTE.commandlines.randomSeeds.sh
@@ -0,0 +1,47 @@
+export BATCHSIZE=5 #2, 3, 5, 2, 5
+export PRETRAINBATCHSIZE=32
+export EPOCHSIZE=20
+export PRETRAINSAMPLESIZE=50
+export LEARNINGRATE=1e-6
+
+CUDA_VISIBLE_DEVICES=0 python -u train.RTE.py \
+    --task_name rte \
+    --do_train \
+    --do_lower_case \
+    --num_train_epochs $EPOCHSIZE \
+    --pretrain_epochs 10 \
+    --pretrain_sample_size $PRETRAINSAMPLESIZE \
+    --train_batch_size $BATCHSIZE \
+    --pretrain_batch_size $PRETRAINBATCHSIZE \
+    --eval_batch_size 32 \
+    --learning_rate $LEARNINGRATE \
+    --max_seq_length 128 \
+    --seed 16 > log.RTE.PretrainEpochs.10.seed.16.txt 2>&1 &
+
+CUDA_VISIBLE_DEVICES=6 python -u train.RTE.py \
+    --task_name rte \
+    --do_train \
+    --do_lower_case \
+    --num_train_epochs $EPOCHSIZE \
+    --pretrain_epochs 10 \
+    --pretrain_sample_size $PRETRAINSAMPLESIZE \
+    --train_batch_size $BATCHSIZE \
+    --pretrain_batch_size $PRETRAINBATCHSIZE \
+    --eval_batch_size 32 \
+    --learning_rate $LEARNINGRATE \
+    --max_seq_length 128 \
+    --seed 32 > log.RTE.PretrainEpochs.10.seed.32.txt 2>&1 &
+
+CUDA_VISIBLE_DEVICES=7 python -u train.RTE.py \
+    --task_name rte \
+    --do_train \
+    --do_lower_case \
+    --num_train_epochs $EPOCHSIZE \
+    --pretrain_epochs 10 \
+    --pretrain_sample_size $PRETRAINSAMPLESIZE \
+    --train_batch_size $BATCHSIZE \
+    --pretrain_batch_size $PRETRAINBATCHSIZE \
+    --eval_batch_size 32 \
+    --learning_rate $LEARNINGRATE \
+    --max_seq_length 128 \
+    --seed 64 > log.RTE.PretrainEpochs.10.seed.64.txt 2>&1 &
